6b6398eac3bb94c549e3f12a3d436eefb541cee7
vishwajeet <vishwajeetsinght@gmail.com>
2021-04-19 15:32:21 +0530

    about us page

---
+++ page-about.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+<div class="w3-row">
+<!-- Blog entries -->
+  <div class="w3-col l8 s12">
+    
+    <div class="w3-card-4 w3-margin w3-white">
+      <img src="<?php echo get_template_directory_uri(); ?>/res/404.jpg" alt="Not Found" loading="lazy" style="width:100%;">
+
+      <div class="w3-container">
+        <div class="w3-justify w3-padding">
+
+          <?php
+          if (have_posts()):
+            while (have_posts()) : the_post();
+              the_content();
+            endwhile;
+          endif;
+          ?>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="w3-col l4" >
+    <?php get_sidebar(); ?>
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
