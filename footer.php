65f8c97252c3761f6f00ed97259a91b60d14589e
vishwajeet <vishwajeetsinght@gmail.com>
2021-04-20 11:38:16 +0530

    announcement text

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 <!-- Footer -->
 <footer class="w3-container w3-theme w3-padding-32 w3-margin-top">
-  <p>&COPY;<?php bloginfo( 'name' ) ?></p>
+  <p>&COPY;&nbsp;<?php bloginfo( 'name' ); echo ', '.date("Y");?></p>
 </footer>
 
 <?php wp_footer() ?>
--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,12 @@
 <div class="w3-card w3-margin w3-margin-top">
     <img src="<?php echo get_template_directory_uri(); ?>/res/coming-soon.jpg" style="width:100%">
     <div class="w3-container w3-white">
-        <h4><b>My Name</b></h4>
-        <p>Just me, myself and I, exploring the universe of uknownment. I have a heart of love and a interest of lorem ipsum and mauris neque quam blog. I want to share my world with you.</p>
+        <h4 class="w3-center"><b>New posts on <span class="w3-xlarge w3-text-theme">Jun 1, 21</span></b></h4>
+        <p class="w3-justify">
+            <span class="w3-xlarge">Hi</span>, We are constantly working to make available high quality blogs on productivity, positivity, motivation and learning on this website.
+            In this journey, we are excited to announce upcoming post on "how to identify and deal with your comfort zone". we would be happy to address
+            any queries around this topic in this blog, please use our contact page to let us know what you expect from this post. 
+        </p>
     </div>
 </div>
 <hr>
